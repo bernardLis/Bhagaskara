694698369bc0f3fa9c793d4985fbae147f1dc80f
bernard lis <bernard lis>
2015-12-10 16:59:34 +0100

    ive done the header and sticky menu

---
+++ js/app.js
@@ -0,0 +1,21 @@
+/**
+ * Created by User on 10/12/2015.
+ */
+$(document).ready(function(){
+
+
+
+    function stickyMenu (){
+        var stickyMenu =  $(".menuSticky");
+        var menuOffsetFromTop = stickyMenu.position().top;
+        $(window).on("scroll", function(){
+            if($(window).scrollTop()>menuOffsetFromTop){
+                stickyMenu.addClass("sticky");
+            }
+            else{
+                stickyMenu.removeClass("sticky");
+            }
+        });
+    }
+    stickyMenu()
+});
