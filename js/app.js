cecbcb15885d32033b18de7ae5cba29dca5879fc
bernard lis <bernard lis>
2015-12-14 00:41:54 +0100

    added a slider section

--- js/app.js
+++ js/app.js
@@ -14,7 +14,31 @@ $(document).ready(function(){
             }
         });
     }
-    stickyMenu()
+    stickyMenu();
+    function testimonialsSlider (){
+        var listWithPictures = $(".testimonialsSlider").find("li");
+        var prevButton = $("#prevButton");
+        var nextButton = $("#nextButton");
+        var visiblePicture = 0;
 
+        listWithPictures.eq(visiblePicture).removeClass("invisible");
 
+        nextButton.on("click", function(){
+           listWithPictures.eq(visiblePicture).addClass("invisible");
+            visiblePicture++;
+            if(visiblePicture >= listWithPictures.length){
+                visiblePicture=0;
+            }
+            listWithPictures.eq(visiblePicture).removeClass("invisible");
+        });
+        prevButton.on("click", function(){
+           listWithPictures.eq(visiblePicture).addClass("invisible");
+            visiblePicture--;
+            if(visiblePicture < 0){
+                visiblePicture = listWithPictures.length-1;
+            }
+            listWithPictures.eq(visiblePicture).removeClass("invisible");
+        });
+    }
+    testimonialsSlider()
 });
